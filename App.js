4f48f15185d243d5babd1f54f848130306d41512
Matt Moore <Matt.M.Moore13@outlook.com>
2021-12-15 18:07:36 -0500

    - Added navigation - Added a new screen

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import HomeScreen from './src/screens/HomeScreen'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hooo boy</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+    <Stack.Navigator
+        screenOptions={{
+    			headerStyle: {
+    				backgroundColor: '#228CDB'
+    			},
+          headerTintColor: '#fff'
+        	}} 
+        initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
