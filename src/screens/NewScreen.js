7c7853d30302ef1f72059134c5f04f4c7a3a6dd7
Matt Moore <Matt.M.Moore13@outlook.com>
2021-12-16 18:39:18 -0500

    - Imported: -- Formik from formik -- useAsyncStorage from @react-native-async-storage/async-storage - Added: -- form and state modifier

--- src/screens/NewScreen.js
+++ src/screens/NewScreen.js
@@ -1,10 +1,53 @@
+import { Formik } from 'formik'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { useAsyncStorage } from '@react-native-async-storage/async-storage'
+import { StyleSheet, View } from 'react-native'
+import { Text } from 'react-native-elements'
+import { Button } from 'react-native-elements/dist/buttons/Button'
+import { Input } from 'react-native-elements/dist/input/Input'
 
 export default function NewScreen () {
+  const { getItem, setItem } = useAsyncStorage('todo')
+  
+  function newTask (values) {
+    //TODO save new task
+  }
+  
+  
   return (
-    <View>
-      <Text>This is a New Screen</Text>
-    </View>
+    <Formik
+      initialValues={{title: ''}}
+      onSubmit={newTask}
+    >
+      {({handleChange, handleBlur, handleSubmit, values}) => (
+        <View style={style.container}>
+          <Text h4>New Todo Item</Text>
+          <Input
+            placeholder="Example: Cook, Clean, etc..."
+            onChangeText={handleChange('title')}
+            onBlur={handleBlur('title')}
+            style={style.input}
+          />
+          <Button 
+            title="Add"
+            onPress={handleSubmit}
+            style={style.button}
+          />
+        </View>
+      )}
+    </Formik>
   )
 }
+
+const style = StyleSheet.create({
+  container: {
+    marginTop: 10,
+    padding: 10
+  },
+  input: {
+    marginTop: 10
+  },
+  button: {
+    backgroundColor: '#228CDB'
+  }
+})
